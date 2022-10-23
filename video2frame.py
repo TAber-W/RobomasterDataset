ed6f2e532c9a9dc98043a0cd955e579f0ebe1ff8
TAber-W <852707293@qq.com>
2022-10-23 17:28:43 +0800

    update

--- video2frame.py
+++ video2frame.py
@@ -18,9 +18,12 @@ def run(video_pth='',frame_pth='',frame_len=1):
     while(1):
         ret,frame = cap.read()
         flag = flag+1
-        if flag == frame_len:
-            #print("正在保存第"+str(num)+"张图片")
-            cv2.imwrite(frame_pth[0]+"/"+str(num)+".jpg", frame)
+        if flag == frame_len[0]:
+            if ret:
+                #print("正在保存第"+str(num)+"张图片")
+                cv2.imwrite(frame_pth[0]+"/"+str(num)+".jpg", frame)
+            else:
+                break
             num = num+1
             flag = 0
     print("共输出"+str(num)+"张图片")
