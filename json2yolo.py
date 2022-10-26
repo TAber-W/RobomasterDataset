e08adc91a240b8ee6b30824fba06e0b0244a30cd
TAber-W <852707293@qq.com>
2022-10-26 10:08:32 +0800

    update

--- json2yolo.py
+++ json2yolo.py
@@ -6,6 +6,8 @@ import jsonpath
 json_path = "kk"
 save_path = "j2y"
 labels_name = []
+yy=[]
+xx=[]
 
 for root, dirs, files in os.walk(json_path, topdown=False):
     for i in range(len(files)):
@@ -23,14 +25,52 @@ for root, dirs, files in os.walk(json_path, topdown=False):
                     f.write(' ')
                     for s in range(len(points[t])):
                         for ss in range (len(points[t][s])):
-                            f.write(str(points[t][s][ss])+' ')
+                            if ss == 0:
+                                #f.write(str(points[t][s][ss]/1280)+' ')
+                                xx.append(points[t][s][ss]/1280)
+                            elif ss == 1:
+                                #f.write(str(points[t][s][ss]/1024)+' ')
+                                yy.append(points[t][s][ss]/1024)
+                    xmax = max(xx)
+                    xmin = min(xx)
+                    ymax = max(yy)
+                    ymin = min(yy)
+                    xcenter = (xmax+xmin)/2
+                    ycenter = (ymax+ymin)/2
+                    w = xmax - xmin
+                    h = ymax - ymin
+                    f.write(str(round(xcenter,6)) + " " + str(round(ycenter,6)) + " " + str(round(w,6)) + " " + str(round(h,6))+" ")
+                    for aa in range(5):
+                        f.write(str(xx[aa])+" ")
+                        f.write(str(yy[aa])+" ")
+                    xx = []
+                    yy = []
                     f.write("\r\n")
                 if label[t] == 'RF':
                     f.write('1')
                     f.write(' ')
                     for s in range(len(points[t])):
                         for ss in range (len(points[t][s])):
-                            f.write(str(points[t][s][ss])+' ')
+                            if ss == 0:
+                                #f.write(str(points[t][s][ss]/1280)+' ')
+                                xx.append(points[t][s][ss]/1280)
+                            elif ss == 1:
+                                #f.write(str(points[t][s][ss]/1020)+' ')
+                                yy.append(points[t][s][ss]/1024)
+                    xmax = max(xx)
+                    xmin = min(xx)
+                    ymax = max(yy)
+                    ymin = min(yy)
+                    xcenter = (xmax+xmin)/2
+                    ycenter = (ymax+ymin)/2
+                    w = xmax - xmin
+                    h = ymax - ymin
+                    f.write(str(round(xcenter,6)) + " " + str(round(ycenter,6)) + " " + str(round(w,6)) + " " + str(round(h,6))+" ")
+                    for aa in range(5):
+                        f.write(str(xx[aa])+" ")
+                        f.write(str(yy[aa])+" ")
+                    xx = []
+                    yy = []
                     f.write("\r\n")
             
            
